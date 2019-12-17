24cbe6ed09a38765aa35081afb43f574cf3947ab
Tetyana <tsergata@gmail.com>
2019-12-17 18:38:48 +0100

    prove calcoli

--- js/script.js
+++ js/script.js
@@ -1,8 +1,16 @@
+// Il tuo nome
 var nomeUtente = prompt('Il tuo nome');
 console.log(nomeUtente);
-
+// Il tuo cognome
 var cognomeUtente = prompt('Il tuo cognome');
 console.log(cognomeUtente);
-
+// Il colore preferito
 var colorePreferito = prompt('Il tuo colore preferito');
 console.log(colorePreferito);
+
+var password = nomeUtente + cognomeUtente + colorePreferito;
+console.log(password);
+
+document.getElementById('nome_utente').innerHTML = nomeUtente;
+document.getElementById('cognome_utente').innerHTML = cognomeUtente;
+document.getElementById('colore_preferito').innerHTML = colorePreferito;
