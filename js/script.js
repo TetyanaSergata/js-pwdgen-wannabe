fb09f8f47a1447a6fe0cd6e203bc5cdedc0963f3
Tetyana <tsergata@gmail.com>
2019-12-17 17:59:33 +0100

    prove javascript

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var nomeUtente = prompt('Il tuo nome');
+console.log(nomeUtente);
+
+var cognomeUtente = prompt('Il tuo cognome');
+console.log(cognomeUtente);
+
+var colorePreferito = prompt('Il tuo colore preferito');
+console.log(colorePreferito);
